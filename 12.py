ed9ee445db742a20c1137d1cf7dcedf8133acfb8
Doronin Yuri <doronin.iua@phystech.edu>
2024-02-19 00:10:58 +0300

    echo bot

--- 12.py
+++ 12.py
@@ -1,1 +1,42 @@
-print(1)
+import requests
+import time
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+    
+API_URL = "https://api.telegram.org/bot"
+BOT_TOKEN = "6864297599:AAHGQDRqB3z9-5VP2R07GBRWhjg_rF2Ai38"
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+cat_resp : requests.Response
+
+bot = Bot(token = BOT_TOKEN)
+dp = Dispatcher()
+
+counter = 0
+MAX_NUM = 100
+offset = -2
+timeout = -15
+updates: dict
+
+
+@dp.message(Command(commands = ["start"]))
+async def start_command(message : Message):
+    await message.answer("salam popolam, send me sth, and i will reply you same text")
+
+@dp.message(Command(commands = ["help"]))
+async def help_command(message : Message):
+    await message.answer("print me something, i will resend you same")
+
+@dp.message()
+async def send_command(message : Message):
+    await message.reply(text = message.text)
+    print(message)
+
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
+                         
+
